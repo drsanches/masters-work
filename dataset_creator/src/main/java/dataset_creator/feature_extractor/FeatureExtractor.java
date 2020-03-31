9d3a806e82290db75e57690f8b119955bce6a6ae
Alexander <dr.sanches3@gmail.com>
2020-03-31 18:12:41 +0300

    Big refactoring with receivedTransactions and sentTransactions

--- dataset_creator/src/main/java/dataset_creator/feature_extractor/FeatureExtractor.java
+++ dataset_creator/src/main/java/dataset_creator/feature_extractor/FeatureExtractor.java
@@ -3,6 +3,7 @@ package dataset_creator.feature_extractor;
 import java.text.DateFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
@@ -18,14 +19,47 @@ public class FeatureExtractor {
     public Map<String, Double> extractFeaturesFromCSV(List<String> csvLines) throws ParseException {
         Map<String, Double> features = new HashMap<>();
         List<String> transactions = csvLines.subList(1, csvLines.size());
+        List<String> receivedTransactions = getReceivedTransactionsList(transactions);
+        List<String> sentTransactions = getSentTransactionsList(transactions);
+
         features.put(FeatureName.AVG_TIME_BETWEEN_TRANS.toString(), getAvgTimeBetweenTrans(transactions));
-        features.put(FeatureName.AVG_TIME_BETWEEN_RECEIVED_TRANS.toString(), getAvgTimeBetweenReceivedTrans(transactions));
-        features.put(FeatureName.AVG_TIME_BETWEEN_SENT_TRANS.toString(), getAvgTimeBetweenSentTrans(transactions));
+        features.put(FeatureName.AVG_TIME_BETWEEN_RECEIVED_TRANS.toString(), getAvgTimeBetweenTrans(receivedTransactions));
+        features.put(FeatureName.AVG_TIME_BETWEEN_SENT_TRANS.toString(), getAvgTimeBetweenTrans(sentTransactions));
         features.put(FeatureName.DEVIATION_TIME_BETWEEN_TRANS.toString(), getDeviationTimeBetweenTrans(transactions));
+        features.put(FeatureName.DEVIATION_TIME_BETWEEN_RECEIVED_TRANS.toString(), getDeviationTimeBetweenTrans(receivedTransactions));
+        features.put(FeatureName.DEVIATION_TIME_BETWEEN_SENT_TRANS.toString(), getDeviationTimeBetweenTrans(sentTransactions));
         return features;
     }
 
+    public List<String> getReceivedTransactionsList(List<String> transactions) {
+        List<String> receivedTransactions = new ArrayList<>();
+        for (String transaction: transactions) {
+            String direction = transaction.split(",")[1];
+            if (direction.equals("IN")) {
+                receivedTransactions.add(transaction);
+            }
+        }
+        return receivedTransactions;
+    }
+
+    public List<String> getSentTransactionsList(List<String> transactions) {
+        List<String> sentTransactions = new ArrayList<>();
+        for (String transaction: transactions) {
+            String direction = transaction.split(",")[1];
+            if (direction.equals("OUT")) {
+                sentTransactions.add(transaction);
+            }
+        }
+        return sentTransactions;
+    }
+
+    /**
+     * @return average time or null if there are less than two transactions
+     * */
     private Double getAvgTimeBetweenTrans(List<String> transactions) throws ParseException {
+        if (transactions.size() < 2) {
+            return null;
+        }
         double N = transactions.size() - 1;
         Date date2 = DATE_FORMAT.parse(transactions.get(0).split(",")[0]);
         Date date1 = DATE_FORMAT.parse(transactions.get(transactions.size() - 1).split(",")[0]);
@@ -33,110 +67,12 @@ public class FeatureExtractor {
     }
 
     /**
-     * @return transaction string or null if there are no received transactions
+     * @return deviation time or null if there are less than two transactions
      * */
-    private String getFirstReceivedTransaction(List<String> transactions) {
-        for (int i = transactions.size() - 1; i >= 0; i--) {
-            String direction = transactions.get(i).split(",")[1];
-            if (direction.equals("IN")) {
-                return transactions.get(i);
-            }
-        }
-        return null;
-    }
-
-    /**
-     * @return transaction string or null if there are no received transactions
-     * */
-    private String getLastReceivedTransaction(List<String> transactions) {
-        for (String line: transactions) {
-            String direction = line.split(",")[1];
-            if (direction.equals("IN")) {
-                return line;
-            }
-        }
-        return null;
-    }
-
-    private int getReceivedTransactionsNumber(List<String> transactions) {
-        int count = 0;
-        for (String line: transactions) {
-            String direction = line.split(",")[1];
-            if (direction.equals("IN")) {
-                count++;
-            }
-        }
-        return count;
-    }
-
-    /**
-     * @return average time or null if there are no received transactions
-     * */
-    private Double getAvgTimeBetweenReceivedTrans(List<String> transactions) throws ParseException {
-        double N = getReceivedTransactionsNumber(transactions);
-        String transaction1 = getFirstReceivedTransaction(transactions);
-        String transaction2 = getLastReceivedTransaction(transactions);
-        if (transaction1 != null && transaction2 != null) {
-            Date date1 = DATE_FORMAT.parse(transaction1.split(",")[0]);
-            Date date2 = DATE_FORMAT.parse(transaction2.split(",")[0]);
-            return (date2.getTime() - date1.getTime()) / N;
-        }
-        return null;
-    }
-
-    /**
-     * @return transaction string or null if there are no sent transactions
-     * */
-    private String getFirstSentTransaction(List<String> transactions) {
-        for (int i = transactions.size() - 1; i >= 0; i--) {
-            String direction = transactions.get(i).split(",")[1];
-            if (direction.equals("OUT")) {
-                return transactions.get(i);
-            }
-        }
-        return null;
-    }
-
-    /**
-     * @return transaction string or null if there are no sent transactions
-     * */
-    private String getLastSentTransaction(List<String> transactions) {
-        for (String line: transactions) {
-            String direction = line.split(",")[1];
-            if (direction.equals("OUT")) {
-                return line;
-            }
-        }
-        return null;
-    }
-
-    private int getSentTransactionsNumber(List<String> transactions) {
-        int count = 0;
-        for (String line: transactions) {
-            String direction = line.split(",")[1];
-            if (direction.equals("OUT")) {
-                count++;
-            }
-        }
-        return count;
-    }
-
-    /**
-     * @return average time or null if there are no sent transactions
-     * */
-    private Double getAvgTimeBetweenSentTrans(List<String> transactions) throws ParseException {
-        double N = getSentTransactionsNumber(transactions);
-        String transaction1 = getFirstSentTransaction(transactions);
-        String transaction2 = getLastSentTransaction(transactions);
-        if (transaction1 != null && transaction2 != null) {
-            Date date1 = DATE_FORMAT.parse(transaction1.split(",")[0]);
-            Date date2 = DATE_FORMAT.parse(transaction2.split(",")[0]);
-            return (date2.getTime() - date1.getTime()) / N;
-        }
-        return null;
-    }
-
     private Double getDeviationTimeBetweenTrans(List<String> transactions) throws ParseException {
+        if (transactions.size() < 2) {
+            return null;
+        }
         double avgTime = getAvgTimeBetweenTrans(transactions);
         double sum = 0;
         Date date1 = DATE_FORMAT.parse(transactions.get(0).split(",")[0]);
