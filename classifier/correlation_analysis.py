Label rotation bug fix

classifier/correlation_analysis.py
@@ -16,7 +16,6 @@ corr = d.corr('kendall')
 
 plt.figure()
 sns.set(style="dark")
-plt.tick_params(axis = 'x', labelrotation = 0)
 
 mask = np.triu(np.ones_like(corr, dtype=np.bool))
 cmap = sns.diverging_palette(220, 10, as_cmap=True)
@@ -26,6 +25,6 @@ sns.heatmap(corr, mask=mask, cmap=cmap, vmax=.3,
 points = [i + 0.5 for i in range(len(features))]
 xticks = [i + 1 for i in range(len(features))]
 yticks = [features[i] + '    ' + str(i + 1) for i in range(len(features))]
-plt.xticks(points, xticks)
+plt.xticks(points, xticks, rotation=0)
 plt.yticks(points, yticks)
 plt.show()